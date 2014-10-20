80c9a91dc75207c46359ae8e65125b83d10576a8
elizabeth <elizabethmort@gmail.com>
2014-10-20 09:49:58 -0700

    some JS exercises

---
+++ sums.js
@@ -0,0 +1,41 @@
+/**
+ * Created by elizabeth on 9/26/14.
+ */
+function add(a, b) {
+    return a + b;
+}
+function multiplication (c, d) {
+    return c * d;
+}
+
+alert ( add (1, 2));
+alert (multiplication(5, 5));
+
+var subA = 5;
+var subB = 2;
+var subtractAB = subA - subB;
+alert (subtractAB);
+
+var multiA = 6;
+var multiB = 9;
+var multiAB = multiA * multiB;
+alert (multiAB);
+
+var comboA = 6;
+var comboB = 9;
+var comboC= 4;
+var comboABC = comboA * comboB + comboC;
+alert (comboABC);
+
+var comboA1 = 6;
+var comboB1 = 9;
+var comboC1= 4;
+var comboABC1 = comboA1 * (comboB1 + comboC1);
+alert (comboABC1);
+
+var addA = 1;
+var addAA = 1;
+var addB = 1;
+var addBB = 1;
+var multiAdd = (addA + addAA) * (addB + addBB);
+alert (multiAdd);
